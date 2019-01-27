58b246063696159c41c9cfef34c228623965c5da
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-01-27 14:56:53 -0200

    StumpWM

---
+++ roles/stumpwm/templates/init.lisp
@@ -0,0 +1,74 @@
+;; -*- mode: lisp -*-
+(ql:quickload :swank)
+(ql:quickload :clx-truetype)
+
+(in-package :stumpwm)
+
+(load-module :cpu)
+(load-module :ttf-fonts)
+(load-module :battery-portable)
+
+;; use super-q
+(set-prefix-key (kbd "s-q"))
+
+;; aliases
+(setf *window-format* "%m%n%s%20c")
+(setf *time-modeline-string* "%e, %a %l:%M")
+(setf *mode-line-timeout* 2)
+(setf *mode-line-border-width* 0)
+
+(setf *screen-mode-line-format*
+      (list "%g %W ^> %C | %B"))
+(enable-mode-line (current-screen) (current-head) t)
+
+;; commands
+(defcommand tmg_runcmd () ()
+  "exec a command"
+  (run-shell-command "bash -l -c 'rofi -show run'"))
+
+(define-key *top-map* (kbd "s-r") "tmg_runcmd")
+
+(defcommand tmg_editor () ()
+  "run or raise emacs"
+  (run-or-raise "emacs" '(:class "Emacs")))
+
+(define-key *top-map* (kbd "s-e") "tmg_editor")
+(define-key *root-map* (kbd "e") "tmg_editor")
+(undefine-key *root-map* (kbd "C-e"))
+
+(defcommand tmg_terminal () ()
+  "run or raise terminal"
+  (run-or-raise "terminator" '(:class "Terminator")))
+
+(define-key *top-map* (kbd "s-t") "tmg_terminal")
+(define-key *root-map* (kbd "c") "tmg_terminal")
+(undefine-key *root-map* (kbd "C-c"))
+
+(defcommand tmg_firefox () ()
+  "run or raise firefox"
+  (run-or-raise "firefox" '(:class "Firefox")))
+
+(define-key *top-map* (kbd "s-b") "tmg_firefox")
+
+(defcommand tmg_passmenu () ()
+  "run passmenu"
+  (run-shell-command "bash -l -c passmenu"))
+
+(define-key *top-map* (kbd "s-p") "tmg_passmenu")
+
+;; defaults
+
+(xft:cache-fonts)
+;; (set-font
+;;  (make-instance 'xft:font :family "Iosevka SS07" :subfamily "Regular" :size 11))
+
+;; (setf *startup-message* nil)
+
+;; (setq *ignore-wm-inc-hints* t)
+
+(setq *window-border-style* :thin)
+
+(setf *mouse-focus-policy* :click)
+
+;; swank server
+(funcall (intern "CREATE-SERVER" :swank) :port 19876 :dont-close t)
