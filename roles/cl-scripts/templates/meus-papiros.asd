faec6751b8d8ae072f407ce909994b562698cd68
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-02-16 18:00:50 -0200

    VLC scripts in Common Lisp.

--- roles/cl-scripts/templates/meus-papiros.asd
+++ roles/cl-scripts/templates/meus-papiros.asd
@@ -9,4 +9,4 @@
   :depends-on ((:version "cl-scripting" "0.1")
                (:version "inferior-shell" "2.0.3.3")
                (:version "fare-utils" "1.0.0.5")
-               "meus-papiros/main"))
+{{ papiros | map('regex_replace', '^(.+)$', '"meus-papiros/\\1"') | join('\n') | indent(width=15, indentfirst=True) }}))
---
+++ roles/cl-scripts/templates/vlc.lisp
@@ -0,0 +1,39 @@
+(uiop:define-package :meus-papiros/vlc
+    (:use #:cl
+          #:uiop
+          #:cl-scripting
+          #:inferior-shell
+          #:fare-utils
+          #:cl-launch/dispatch)
+  (:export #:tmg-vlc))
+
+;; (asdf:load-system :meus-papiros)
+
+(in-package #:meus-papiros/vlc)
+
+(defvar *vlc-host*
+  "localhost")
+
+(defvar *vlc-port*
+  "8080")
+
+(defun get-pass ()
+  (string-trim '(#\Newline) (run/s `(pass local/vlc/password))))
+
+(defun call-vlc (command)
+  (let* ((auth (get-pass))
+        (curl-comm (format nil "curl -H 'content-type: text/xml' -XGET -u':~A' '~A:~A/requests/status.xml?command=~A'" auth *vlc-host* *vlc-port* command)))
+    (run/nil `(echo ,curl-comm))))
+
+(exporting-definitions
+  (defun tmg-vlc (&optional command)
+    (cond
+      ((equal command "next") (call-vlc "pl_next"))
+      ((equal command "previous") (call-vlc "pl_previous"))
+      ((equal command "pause-resume") (call-vlc "pl_pause"))
+      ('t (progn
+            (run `(echo "comandos possíveis: next, previous, pause-resume"))
+            (failure))))
+    (success)))
+
+(register-commands :meus-papiros/vlc)
