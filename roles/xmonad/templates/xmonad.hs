94e6dfd68901f90a651b7b669d5a684cf31ae306
Thales Macedo Garitezi <thalesmg@gmail.com>
2019-02-16 16:10:25 -0200

    Xmonad.

---
+++ roles/xmonad/templates/xmonad.hs
@@ -0,0 +1,108 @@
+import qualified Data.Map as M
+import Data.Map (Map)
+import Graphics.X11.ExtraTypes.XF86
+import System.IO
+import XMonad
+import XMonad.Actions.SpawnOn (spawnOn, manageSpawn)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout
+import XMonad.Layout.Circle (Circle (..))
+import XMonad.Layout.Cross (simpleCross)
+import XMonad.Layout.Tabbed (simpleTabbed)
+import XMonad.StackSet (greedyView, shift, view)
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
+
+
+myModMask :: KeyMask
+myModMask = mod4Mask
+
+altMask = mod1Mask
+
+myBorderWidth :: Dimension
+myBorderWidth = 1
+
+myWorkspaces :: [WorkspaceId]
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] ++ map snd myExtraWorkspaces
+
+myExtraWorkspaces = [(xK_0, "0"), (xK_minus, "-"), (xK_equal, "=")]
+
+extraWorkspaceKeys conf@XConfig{modMask = modm} = M.fromList $
+  [
+    ((modm, key), windows $ greedyView workspace)
+    | (key, workspace) <- myExtraWorkspaces
+  ] ++ [
+    ((modm .|. shiftMask, key), windows $ shift workspace)
+    | (key, workspace) <- myExtraWorkspaces
+  ] ++ [
+    ((modm, stringToKeysym $ "KP_" ++ show n), windows $ greedyView (show n))
+    | n <- [1..9]
+  ]
+  -- ++ [
+  --   ((modm .|. modifier, key), screenWorkspace screen >>= flip whenJust (windows . f))
+  --   | (key, screen) <- zip [xK_w, xK_e, xK_r] [1, 0, 2]
+  --   , (f, modifier) <- [(view, 0), (shift, shiftMask)]
+  -- ]
+
+myMiscKeys conf@XConfig{modMask = modm} = M.fromList $
+  [ ((modm, xK_p), spawn "dmenu_run")
+  , ((modm, xK_b), sendMessage ToggleStruts)
+  , ((altMask .|. controlMask, xK_l), spawn "xscreensaver-command --lock")
+  , ((shiftMask .|. modm, xK_Print), spawn "peek")
+  , ((modm, xK_Print), spawn "deepin-screenshot -s {{ home }}/Pictures")
+  , ((0, xK_Print), spawn "{{ home }}/bin/take-screenshot.sh")
+  , ((0, xF86XK_MonBrightnessDown), spawn "{{ home }}/bin/backlight/manage_backlight.sh dec")
+  , ((0, xF86XK_MonBrightnessUp), spawn "{{ home }}/bin/backlight/manage_backlight.sh inc")
+  , ((modm .|. controlMask, xK_KP_Multiply), spawn "{{ home }}/bin/tmg-vlc.sh next")
+  , ((0, xF86XK_AudioNext), spawn "{{ home }}/bin/tmg-vlc.sh next")
+  , ((modm .|. controlMask, xK_KP_Divide), spawn "{{ home }}/bin/tmg-vlc.sh previous")
+  , ((0, xF86XK_AudioPrev), spawn "{{ home }}/bin/tmg-vlc.sh previous")
+  , ((modm .|. controlMask, xK_KP_Delete), spawn "{{ home }}/bin/tmg-vlc.sh pause-resume")
+  , ((0, xF86XK_AudioPlay), spawn "{{ home }}/bin/tmg-vlc.sh pause-resume")
+  ]
+
+myKeys conf = foldMap ($ conf) [myMiscKeys, extraWorkspaceKeys, keys def]
+
+myStartupHook :: X ()
+myStartupHook = do
+  -- spawnOn "1" "google-chrome"
+  -- spawnOn "3" "emacs25"
+  setWMName "LG3D"
+  spawn "{{ home }}/bin/tmg-xinit.sh"
+  return ()
+
+myManageHook :: ManageHook
+myManageHook = composeAll [ isFullscreen --> doFullFloat
+                          ]
+               <+> composeOne [ isDialog -?> doCenterFloat
+                              ]
+
+myLayoutHook = avoidStruts $
+               (layoutHook def)
+               -- ||| Circle
+               -- ||| simpleCross
+               ||| simpleTabbed
+
+myConfig xmobarProc = def
+                       { modMask = myModMask
+                       , borderWidth = myBorderWidth
+                       , workspaces = myWorkspaces
+                       , manageHook = manageSpawn
+                                      <+> manageDocks
+                                      <+> myManageHook
+                                      <+> manageHook def
+                       , layoutHook = myLayoutHook
+                       , startupHook = myStartupHook >> startupHook def
+                       , logHook = dynamicLogWithPP xmobarPP { ppOutput = hPutStrLn xmobarProc }
+                       , handleEventHook = handleEventHook def
+                                           <+> docksEventHook
+                       , keys = myKeys
+                       , terminal = "terminator"
+                       } -- `additionalKeys` extraWorkspaceKeys
+
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ myConfig xmproc
