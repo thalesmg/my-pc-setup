1f278818276a09778db04b3dbf4468f7e7e98316
Thales Macedo Garitezi <thales@zenfinance.com.br>
2019-05-29 15:45:23 -0300

    dbus: seek

--- roles/cl-scripts/templates/dbus.lisp
+++ roles/cl-scripts/templates/dbus.lisp
@@ -7,23 +7,28 @@
           #:cl-launch/dispatch)
   (:export #:tmg-mp-play-pause
            #:tmg-mp-next
-           #:tmg-mp-prev))
+           #:tmg-mp-prev
+           #:tmg-mp-fwd
+           #:tmg-mp-bwd))
 
 ;; (asdf:load-system :meus-papiros)
 
 (in-package #:meus-papiros/dbus)
 
-(defun dbus.mplayer/send (target method)
+(defun dbus.mplayer/send (target method &rest args)
   (let* ((fmt-str (format nil
                           "~{~A~^ ~}"
-                          '("dbus-send"
-                            "--session"
-                            "--dest=org.mpris.MediaPlayer2.~A"
-                            "--type=method_call"
-                            "--print-reply"
-                            "/org/mpris/MediaPlayer2"
-                            "org.mpris.MediaPlayer2.Player.~A")))
-                 (command (format nil fmt-str target method)))
+                          (concatenate
+                           'list
+                           '("dbus-send"
+                             "--session"
+                             "--dest=org.mpris.MediaPlayer2.~A"
+                             "--type=method_call"
+                             "--print-reply"
+                             "/org/mpris/MediaPlayer2"
+                             "org.mpris.MediaPlayer2.Player.~A")
+                           args)))
+         (command (format nil fmt-str target method)))
     (run command)))
 
 (defun dbus.mplayer/play-pause (target)
@@ -35,6 +40,12 @@
 (defun dbus.mplayer/previous (target)
   (dbus.mplayer/send target "Previous"))
 
+(defun dbus.mplayer/forward (target)
+  (dbus.mplayer/send target "Seek" "int64:10000000"))
+
+(defun dbus.mplayer/backward (target)
+  (dbus.mplayer/send target "Seek" "int64:-10000000"))
+
 (exporting-definitions
  (defun tmg-mp-play-pause (target)
    (dbus.mplayer/play-pause target))
@@ -43,6 +54,12 @@
    (dbus.mplayer/next target))
 
  (defun tmg-mp-prev (target)
-   (dbus.mplayer/previous target)))
+   (dbus.mplayer/previous target))
+
+ (defun tmg-mp-fwd (target)
+   (dbus.mplayer/forward target))
+
+ (defun tmg-mp-bwd (target)
+   (dbus.mplayer/backward target)))
 
 (register-commands :meus-papiros/dbus)
